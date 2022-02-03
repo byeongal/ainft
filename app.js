b4528e7a8f2cceaad6537c908c6cc4937927193b
YoungJae Kim <byeongal@comcom.ai>
2022-02-03 12:52:45 +0900

    add data and update code

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 const express = require('express');
+const fs = require('fs');
 const axios = require('axios');
 
 const app = express()
-const gpt_j_endpoint = 'https://eleuther-ai-gpt-j-6b-float16-text-generation-api-ainize-team.endpoint.ainize.ai/predictions/text-generation'
+console.log('Read Data');
+const data = fs.readFileSync(process.env.DATA || './data.txt', 'utf-8');
+console.log('Read Data Done');
+
+const endpoint = process.env.ENDPOINT;
 
 app.use(express.json());
 
-app.set('port', process.env.PORT || 3000)
-;
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', (req, res) => {
-    res.send('Hello, Express');
+    res.send({
+        data
+    })
 })
+
+const processingResponse = (responseText) => {
+    let retText = "";
+    for(let i = 0;i<responseText.length;i++){
+        if(responseText[i] === "\n" || responseText.substr(i,i + 7) === "Human: " || responseText.substr(i, i + 4) === "AI: ")
+            break
+        retText += responseText[i]
+    }
+    return retText.trim();
+}
 
 app.post('/chat', async (req, res) => {
     const {text_inputs} = req.body;
-    const t = await axios.post(gpt_j_endpoint, {
-        text_inputs,
+    const prompt = `${data}\nHuman: ${text_inputs}\nAI:`
+    const t = await axios.post(endpoint, {
+        text_inputs: prompt,
         temperature: 0.9,
         top_p: 0.95,
         repetition_penalty: 0.8,
@@ -23,6 +41,11 @@ app.post('/chat', async (req, res) => {
         top_k: 50,
         length: 50
     });
-    console.log(t);
-    res.send(t);
+    const answer = processingResponse(t.data[0].substr(prompt.length));
+    console.log(answer);
+    res.send({text:answer});
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`app listening on port ${app.get('port')}`);
 });
