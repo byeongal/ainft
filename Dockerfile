d41aae42ec487a16c785d88197528af471bd6aa9
YoungJae Kim <byeongal@comcom.ai>
2022-02-03 17:42:24 +0900

    update code

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM node:16.13.2-alpine
 
-ARG PORT=3000
-ENV PORT="${PORT}"
-
 COPY . /app
 WORKDIR /app
 
--- app.js
+++ app.js
@@ -2,27 +2,44 @@ const express = require('express');
 const fs = require('fs');
 const axios = require('axios');
 
+/*
+Load ENV
+ */
+const dataFilePath = process.env.DATA_FILE_PATH || 'data.txt';
+const endpoint = process.env.ENDPOINT || 'https://eleuther-ai-gpt-j-6b-float16-text-generation-api-ainize-team.endpoint.ainize.ai/predictions/text-generation';
+const port = process.env.PORT || 3000;
+
+/*
+Load Data for AINFT ChatBot
+ */
+const data = fs.readFileSync(dataFilePath, 'utf-8');
+
 const app = express()
-console.log('Read Data');
-const data = fs.readFileSync(process.env.DATA || './data.txt', 'utf-8');
-console.log('Read Data Done');
-
-const endpoint = process.env.ENDPOINT;
-
 app.use(express.json());
 
-app.set('port', process.env.PORT || 3000);
-
+/*
+Get Metadata
+ */
 app.get('/', (req, res) => {
     res.send({
-        data
+        inferenceParameters: {
+            temperature: 0.9,
+            top_p: 0.95,
+            repetition_penalty: 0.8,
+            do_sample: true,
+            top_k: 50,
+            length: 50
+        }
     })
 })
 
+/*
+Postprocessing for ChatBot
+ */
 const processingResponse = (responseText) => {
     let retText = "";
-    for(let i = 0;i<responseText.length;i++){
-        if(responseText[i] === "\n" || responseText.substr(i,i + 7) === "Human: " || responseText.substr(i, i + 4) === "AI: ")
+    for (let i = 0; i < responseText.length; i++) {
+        if (responseText[i] === "\n" || responseText.substr(i, i + 7) === "Human: " || responseText.substr(i, i + 4) === "AI: ")
             break
         retText += responseText[i]
     }
@@ -32,7 +49,7 @@ const processingResponse = (responseText) => {
 app.post('/chat', async (req, res) => {
     const {text_inputs} = req.body;
     const prompt = `${data}\nHuman: ${text_inputs}\nAI:`
-    const t = await axios.post(endpoint, {
+    const responseData = await axios.post(endpoint, {
         text_inputs: prompt,
         temperature: 0.9,
         top_p: 0.95,
@@ -41,11 +58,10 @@ app.post('/chat', async (req, res) => {
         top_k: 50,
         length: 50
     });
-    const answer = processingResponse(t.data[0].substr(prompt.length));
-    console.log(answer);
-    res.send({text:answer});
+    const responseText = responseData.data[0].substr(prompt.length);
+    res.send({text: processingResponse(responseText)});
 });
 
-app.listen(app.get('port'), () => {
+app.listen(port, () => {
     console.log(`app listening on port ${app.get('port')}`);
 });
